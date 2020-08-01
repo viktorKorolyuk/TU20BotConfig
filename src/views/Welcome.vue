b42f2080704bb09290d96bfaf481aa94fabe5a3c
1whatleytay <1whatleytay@hdsb.ca>
2020-08-01 16:09:57 -0400

    Add welcome logs v1

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="max-w-4xl mx-auto px-4">
+  <div class="max-w-4xl mx-auto px-4 mb-32">
     <div class="mb-4">
-      <label class="text-2xl font-bold block mb-1">Message Channel Id </label>
+      <label class="text-2xl font-bold block mb-1">Welcome Channel</label>
       <DiscordSelector
         :options="channelOptions"
         v-model="channel"
@@ -24,6 +24,56 @@
       </textarea>
     </div>
 
+    <div class="mb-4">
+      <label class="text-2xl font-bold block mb-1">
+        Join Logs
+      </label>
+      <div class="">
+        <div
+          v-if="!logs.length"
+          class="border rounded text-center text-gray-600"
+        >
+          Logs are empty...
+        </div>
+
+        <table v-else class="table-auto w-full">
+          <thead>
+            <tr class="text-gray-700">
+              <th class="px-2 py-2 border w-1/6">Id</th>
+              <th class="px-2 py-2 border w-1/3">Username</th>
+              <th class="px-2 py-2 border w-1/6">Nickname</th>
+              <th class="px-2 py-2 border w-1/3">Joined At</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(log, index) of logs"
+              :key="index"
+              class="text-sm"
+              :class="{
+                'bg-red-100': log.leftServer,
+                'bg-gray-100': !log.leftServer && index % 2 === 1
+              }"
+            >
+              <th class="border px-4 py-2 font-medium">{{ log.id }}</th>
+              <th class="border px-4 py-2" v-if="log.leftServer" colspan="2">
+                User Left Server
+              </th>
+              <th class="border px-4 py-2 font-bold" v-if="!log.leftServer">
+                {{ log.username }}#{{ log.discriminator }}
+              </th>
+              <th class="border px-4 py-2 font-medium" v-if="!log.leftServer">
+                {{ log.nickname || '' }}
+              </th>
+              <th class="border px-4 py-2 font-medium">
+                {{ formatDate(log.joinDate) }}
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
     <div class="text-sm text-gray-600">* Settings are auto-saved.</div>
   </div>
 </template>
@@ -34,6 +84,7 @@ import DiscordSelector from '@/components/DiscordSelector'
 
 // Libraries
 import axios from 'axios'
+import { format } from 'date-fns'
 
 export default {
   name: 'Welcome',
@@ -45,7 +96,9 @@ export default {
       channel: null,
       messages: '',
 
-      channelOptions: []
+      channelOptions: [],
+
+      logs: []
     }
   },
 
@@ -62,6 +115,7 @@ export default {
 
     this.channel = this.channelOptions.find(x => x.id === id)
     this.messages = (await axios.get('/api/welcome/messages')).data.join('\n')
+    this.logs = (await axios.get('/api/welcome/logs')).data
   },
 
   methods: {
@@ -71,6 +125,10 @@ export default {
 
     async saveMessages() {
       await axios.put('/api/welcome/messages', this.messages.split('\n'))
+    },
+
+    formatDate(date) {
+      return format(new Date(date), 'MMMM do yyyy h:mm a')
     }
   }
 }
